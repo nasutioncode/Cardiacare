dabac8e4782ba239a434d3d0a8dc7cb6a154dcf3
nasutioncode <nasutioncode@gmail.com>
2019-12-07 12:23:38 +0700

    Step Sampai profil

--- app/src/main/java/com/ghuroba/cardiacare/MainActivity.java
+++ app/src/main/java/com/ghuroba/cardiacare/MainActivity.java
@@ -1,8 +1,12 @@
 package com.ghuroba.cardiacare;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,16 +21,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logout = (Button) findViewById(R.id.logout);
 
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Intent inToLoginPage = new Intent(MainActivity.this, SigInActivity.class);
-                startActivity(inToLoginPage);
-            }
-        });
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_profil:
+                            selectedFragment = new ProfilFragment();
+                            break;
+                        case R.id.nav_notification:
+                            selectedFragment = new NotificationFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
---
+++ app/src/main/java/com/ghuroba/cardiacare/ProfilFragment.java
@@ -0,0 +1,36 @@
+package com.ghuroba.cardiacare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProfilFragment extends Fragment {
+
+    Button logout;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profil,container,false);
+
+
+//        logout = (Button) getActivity().findViewById(R.id.logout);
+//
+//        logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FirebaseAuth.getInstance().signOut();
+//                Intent inToLoginPage = new Intent(getActivity(ProfilFragment).this, SigInActivity.class);
+//                startActivity(inToLoginPage);
+//            }
+//        });
+    }
+}
