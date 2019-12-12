1af8a18d95b83e22dfd171d9013ddfcefbc50200
Muhammad Alifa Rahmatulloh <alifarahmatulloh@gmail.com>
2019-12-12 08:38:48 +0700

    Merge branch 'master' of D:\1.Belajar\1.Coding\Java(AndroidStudio)\0.GITHUB\cardiacare-PAB2019 with conflicts.

--- app/src/main/java/com/ghuroba/cardiacare/CreateDiagnosaActivity.java
+++ app/src/main/java/com/ghuroba/cardiacare/CreateDiagnosaActivity.java
@@ -4,54 +4,164 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateDiagnosaActivity extends AppCompatActivity {
+
+    DatabaseReference databaseReference;
+    FirebaseDatabase firebaseDatabase;
+    FirebaseAuth firebaseAuth;
+
+    RadioGroup rgDiabetes, rgKelamin, rgPerokok;
+    RadioButton rbDiabetesYes, rbDiabetesNo, rbKelaminPria, rbKelaminWanita, rbRokokYes, rbRokokNo;
+    EditText etUsia, etTensi;
+    Spinner spinKolesterol;
+    Button btHasil;
+
+    String diabetes = "";
+    String kelamin = "";
+    String rokok = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_diagnosa);
 
-    }
+        rgDiabetes = findViewById(R.id.radio_diabetes);
+        rgKelamin = findViewById(R.id.radio_kelamin);
+        rgPerokok = findViewById(R.id.radio_rokok);
+        rbDiabetesYes = (RadioButton) findViewById(R.id.diabetesYes);
+        rbDiabetesNo = (RadioButton) findViewById(R.id.diabetesNo);
+        rbKelaminPria = (RadioButton) findViewById(R.id.pria);
+        rbKelaminWanita = (RadioButton) findViewById(R.id.wanita);
+        rbRokokYes = (RadioButton) findViewById(R.id.rokokYes);
+        rbRokokNo = (RadioButton) findViewById(R.id.rokokNo);
+        etUsia = (EditText) findViewById(R.id.text_usia);
+        etTensi = (EditText) findViewById(R.id.text_tensi);
+        spinKolesterol = (Spinner) findViewById(R.id.spinner_kolesterol);
+        btHasil = (Button) findViewById(R.id.button_hasil);
 
-    public void onRadioButtonClicked(View view) {
-        boolean checked =((RadioButton) view).isChecked();
+        final Spinner kolesterol = spinKolesterol;
 
-        switch (view.getId()){
-            case R.id.diabetesYes:
-                break;
-            case R.id.diabetesNo:
-                break;
-            default:
-                break;
-        }
+        List<String> categories = new ArrayList<>();
+        categories.add(0, "4 = 154,44");
+        categories.add(1, "5 = 193,05");
+        categories.add(2, "6 = 231,66");
+        categories.add(3, "7 = 270,27");
+        categories.add(4, "8 = 308,88");
 
-        switch (view.getId()){
-            case R.id.rokokYes:
-                break;
-            case R.id.rokokNo:
-                break;
-             default:
-                 break;
-        }
-    }
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, categories);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        kolesterol.setAdapter(adapter);
 
-    public void showAlert(View view) {
-        AlertDialog.Builder myAlertDialog = new AlertDialog.Builder(CreateDiagnosaActivity.this);
+        databaseReference = FirebaseDatabase.getInstance().getReference("dataDiagnosa");
 
-        myAlertDialog.setTitle("PERINGATAN");
-        myAlertDialog.setMessage("Apakah anda yakin data yang anda isikan sudah benar?");
-        myAlertDialog.setPositiveButton("YA", new DialogInterface.OnClickListener() {
+        btHasil.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
+            public void onClick(View view) {
+                if (rbDiabetesYes.isChecked()){
+                    diabetes = "1";
+                } else if (rbDiabetesNo.isChecked()){
+                    diabetes = "2";
+                }
+
+                if (rbKelaminPria.isChecked()){
+                    kelamin = "1";
+                } else if (rbKelaminWanita.isChecked()){
+                    kelamin = "2";
+                }
+
+                if (rbRokokYes.isChecked()){
+                    rokok = "1";
+                } else if (rbRokokNo.isChecked()){
+                    rokok = "2";
+                }
+
+                final String usia = etUsia.getText().toString();
+                final String tensi = etTensi.getText().toString();
+
+                if (usia.isEmpty()) {
+                    etUsia.setError("Tolong masukkan usia anda");
+                    etUsia.requestFocus();
+                }
+                if (tensi.isEmpty()) {
+                    etTensi.setError("Tolong masukkan tekanan darah anda");
+                    etTensi.requestFocus();
+                }
+
+                //
+                final String kolesterol = spinKolesterol.getSelectedItem().toString();
+
+                //AlertPopUp
+                AlertDialog.Builder myAlertDialog = new AlertDialog.Builder(CreateDiagnosaActivity.this);
+
+                myAlertDialog.setTitle("PERINGATAN");
+                myAlertDialog.setMessage("Apakah anda yakin data yang anda isikan sudah benar?");
+                myAlertDialog.setPositiveButton("YA", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                myAlertDialog.setNegativeButton("TIDAK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+                myAlertDialog.show();
             }
         });
-        myAlertDialog.setNegativeButton("TIDAK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-            }
-        });
+    }
+
+
+
+
+/*
+* if (RadioGroupDiabetes == 1 %% RadioGroupKelamin == 1 %% RadioGroupPerokok == 1 %% tekanan darah >= 40 %%)
+* */
+//    public void onRadioButtonClicked(View view) {
+//        boolean checked =((RadioButton) view).isChecked();
+//
+//        switch (view.getId()){
+//            case R.id.diabetesYes:
+//                break;
+//            case R.id.diabetesNo:
+//                break;
+//            default:
+//                break;
+//        }
+//
+//        switch (view.getId()){
+//            case R.id.rokokYes:
+//                break;
+//            case R.id.rokokNo:
+//                break;
+//             default:
+//                 break;
+//        }
+//    }
+
+    public void buttonHasil(View view) {
+
+//        // RGD = RadioGroupDiabetes
+//        // RGK = RadioGroupKelamin
+//        // RGP = RadioGroupPerokok
+//        int RGD = rgDiabetes.getCheckedRadioButtonId();
+//        rbDiabetes = findViewById(RGD);
+//        int;
     }
 }
---
+++ app/src/main/java/com/ghuroba/cardiacare/Diagnosa.java
@@ -0,0 +1,4 @@
+package com.ghuroba.cardiacare;
+
+public class Diagnosa {
+}
